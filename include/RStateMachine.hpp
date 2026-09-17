e6fb29d17a47ce59fd6e873c2b08ed5d4a096c3e
Kevin <kmarrero1993@gmail.com>
2026-09-17 18:04:40 +0200

    feat/requestStateMachine: statemachine defined

---
+++ include/RStateMachine.hpp
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RStateMachine.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmarrero <kmarrero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/17 16:31:07 by kmarrero          #+#    #+#             */
+/*   Updated: 2026/09/17 17:44:39 by kmarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef REQUEST_STATE_MACHINE
+# define REQUEST_STATE_MACHINE
+
+# include "Webserv.hpp"
+
+enum RequestState
+{
+	REQ_FEED,
+	REQ_LINE,
+	REQ_HEADERS,
+	REQ_BODY,
+	REQ_COMPLETE,
+	REQ_ERROR
+};
+
+enum	RequestEvent
+{
+	REQ_READ,
+	REQ_KEYWORD,
+	END_EVENT
+};
+
+struct	Context
+{
+	RequestState	state;
+	std::string		buffer;
+	std::string		error;
+};
+
+typedef	RequestState	(Request::*ActionFunction)(Context&);
+
+struct	Action
+{
+	ActionFunction	function;
+};
+
+typedef	std::pair<RequestState, RequestEvent>	TransitionKey;
+
+class	RStateMachine
+{
+	private:
+		RequestState	initialState;
+		RequestState	currentState;
+		std::map<TransitionKey, Action>	functions;
+		void			setCurrentState(RequestState state);
+	public:
+		RStateMachine();
+		RStateMachine(RequestState initialState);
+		RStateMachine(const RStateMachine& other);
+		~RStateMachine();
+		RStateMachine&	operator=(const RStateMachine& other);
+		void	addTransition(RequestState fromState,
+								RequestEvent event,
+								RequestEvent toState,
+								ActionFunction function);
+		Action	nextTransition(RequestState fromState, RequestEvent event);
+		void	handle(Context& ctx, Request& request, RequestEvent event);
+		RequestState	getCurrentState();
+		RequestEvent	getNextEvent(RequestState fromState);
+};
+
+#endif
---
+++ src/RStateMachine.cpp
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RStateMachine.cpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmarrero <kmarrero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/17 17:07:17 by kmarrero          #+#    #+#             */
+/*   Updated: 2026/09/17 17:44:36 by kmarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "RStateMachine.hpp"
+
+RStateMachine::RStateMachine()
+	:initialState(REQ_FEED)
+{}
+
+RStateMachine::RStateMachine(RequestState initialState)
+{
+	this->initialState = initialState;
+	this->currentState = initialState;
+}
+
+RStateMachine::RStateMachine(const RStateMachine& other)
+{
+	*this = other;
+}
+
+RStateMachine::~RStateMachine()
+{}
+
+RequestState	RStateMachine::getCurrentState()
+{
+	return (this->currentState);
+}
+
+RequestEvent	RStateMachine::getNextEvent(RequestState fromState)
+{
+	switch (fromState)
+	{
+		case REQ_FEED:
+			return (REQ_READ);
+		case REQ_LINE:
+			return (REQ_KEYWORD);
+		default:
+			return (END_EVENT);
+	}
+}
+
+RStateMachine&	RStateMachine::operator=(const RStateMachine& other)
+{
+	if (this != &other)
+	{
+		this->initialState = other.initialState;
+		this->currentState = other.currentState;
+		this->functions = other.functions;
+	}
+	return (*this);
+}
+
+void	RStateMachine::addTransition(RequestState fromState,
+										RequestEvent event,
+										RequestEvent toState,
+										ActionFunction function)
+{
+	TransitionKey	key = std::make_pair(fromState, event);
+	Action			action;
+
+	action.function = function;
+	this->functions[key] = action;
+}
+
+Action	RStateMachine::nextTransition(RequestState fromState, RequestEvent event)
+{
+	TransitionKey	key = std::make_pair(fromState, event);
+	Action			invalid;
+
+	std::map<TransitionKey, Action>::iterator it = functions.find(key);
+	if (it != functions.end())
+		return (it->second);
+	else
+	{
+		std::cerr << "Invalid transition" << std::endl;
+		invalid.function = NULL;
+		return (invalid);
+	}
+}
+
+void	RStateMachine::handle(Context& ctx, Request& request, RequestEvent event)
+{
+	RequestState	answer;
+	RequestState	currentState = this->getCurrentState();
+	Action			action = this->nextTransition(currentState, event);
+
+	answer = (request.*action.function)(ctx);
+	if (answer == REQ_ERROR)
+	{
+		std::cerr << "In the line: "
+		<< ctx.buffer
+		<<  " " << ctx.error << std::endl;
+		setCurrentState(REQ_ERROR);
+	}
+	setCurrentState(answer);
+}
